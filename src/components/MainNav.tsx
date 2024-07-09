e5205d59d9de609a59bcfcc5b2ba0001f090287e
Leah Nyambura <leahnyambura682@gmail.com>
2024-07-09 23:05:36 +0300

    Reponsive landing page

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,8 +1,11 @@
 import { Button } from "./ui/button";
 
 
-export default function MainNav() {
+const MainNav=()=> {
   return (
-    <Button variant="ghost" className="font-bold hover:text-orange-500 hover:bg-white"> Log In</Button>
+    <Button variant="ghost" className="font-bold hover:text-orange-500 hover:bg-white"> 
+    Log In
+    </Button>
   )
-}
+};
+export default MainNav;
--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,6 +1,8 @@
 
 import React from 'react';
 import Header from '../components/Header'; // Ensure the Header component is imported
+import Hero from '@/components/Hero';
+import Footer from '@/components/Footer';
 
 type Props = {
     children: React.ReactNode;
@@ -10,9 +12,11 @@ const Layout = ({ children }: Props) => {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
+            <Hero/>
             <div className="container mx-auto flex-1 py-10">
                 {children}
             </div>
+            <Footer/>
         </div>
     );
 }
